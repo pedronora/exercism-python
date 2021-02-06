54de35fe91f96ca126c33f0cb2dadfbe1c7c2593
Pedro Nora <pedro@nora.vc>
2021-02-06 18:12:20 -0300

    matrix

--- matrix/matrix.py
+++ matrix/matrix.py
@@ -1,9 +1,20 @@
 class Matrix:
     def __init__(self, matrix_string):
-        pass
+        self.rows = []
+        
+        # Create rows
+        input = matrix_string.split('\n')
+        for row in input:
+            new_row = row.split(' ')
+            new_row = [int(n) for n in new_row]
+            self.rows.append(new_row)
+        
+        # Create columns
+        self.columns = list(map(list, zip(*self.rows)))
+
 
     def row(self, index):
-        pass
+        return self.rows[index-1]
 
     def column(self, index):
-        pass
+        return self.columns[index-1]
