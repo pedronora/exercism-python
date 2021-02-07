fb9706956e10ff5a9776a74e886169afe8504902
Pedro Nora <pedro@nora.vc>
2021-02-07 08:02:06 -0300

    hamming

---
+++ hamming/hamming.py
@@ -0,0 +1,10 @@
+def distance(strand_a, strand_b):
+    if len(strand_a) != len(strand_b):
+        raise ValueError("The strand length are different.")
+
+    distance = 0
+    for n, x in enumerate(strand_a):
+        if x != strand_b[n]:
+            distance +=1
+    
+    return distance
