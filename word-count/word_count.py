ad4869758a7f335878bd3d75ffdc6564c5ce7593
Pedro Nora <pedro@nora.vc>
2021-02-09 10:00:51 -0300

    word_count

---
+++ word-count/word_count.py
@@ -0,0 +1,13 @@
+def count_words(sentence):
+    phrase = map(lambda c: c.lower() if c.isalpha() or c.isdigit() or (c == '\'') else " ", sentence)
+
+    phrase = ''.join(phrase)
+   
+    counts = {}
+    for word in phrase.split():
+        w = word.strip('\'')
+        if w not in counts.keys():
+            counts[w] = 1
+        else:
+            counts[w] += 1
+    return counts
