3bbe16a7de6b93b3f4bbb84c30426b0b622164ca
Pedro Nora <pedro@nora.vc>
2021-02-11 18:02:04 -0300

    triangle

--- triangle/triangle.py
+++ triangle/triangle.py
@@ -1,22 +1,17 @@
 def is_triangle(sides):
-    if abs(sides[0] - sides[1]) < sides[2] and sides[2] < (sides[0] + sides[1]):
-        return True
-    return False
+    return abs(sides[0] - sides[1]) < sides[2] and sides[2] < (sides[0] + sides[1])
 
 def equilateral(sides):
     if is_triangle(sides):
-        if sides[0] == sides[1] and sides[1] == sides[2]:
-            return True
+        return sides[0] == sides[1] and sides[1] == sides[2]
     return False
 
 def isosceles(sides):
     if is_triangle(sides):
-        if sides[0] == sides[1] or sides[1] == sides[2] or sides[2] == sides[0]:
-            return True
+        return sides[0] == sides[1] or sides[1] == sides[2] or sides[2] == sides[0]
     return False
 
 def scalene(sides):
     if is_triangle(sides):
-        if sides[0] != sides[1] and sides[1] != sides[2] and sides[2] != sides[0]:
-            return True
+        return sides[0] != sides[1] and sides[1] != sides[2] and sides[2] != sides[0]
     return False
