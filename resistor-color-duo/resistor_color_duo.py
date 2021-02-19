f0f71bf88f3fef98224d1345e28052d51e752695
Pedro Nora <pedro@nora.vc>
2021-02-19 10:23:24 -0300

    resistor_color_duo

--- resistor-color-duo/resistor_color_duo.py
+++ resistor-color-duo/resistor_color_duo.py
@@ -1,4 +1,4 @@
 def value(colors):
     all_colors = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"]
 
-    return int(''.join([str(all_colors.index(color)) for color in colors[:2]]))
+    return all_colors.index(colors[0])*10 + all_colors.index(colors[1])
