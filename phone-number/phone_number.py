011d9b8176374bb97f66a2d877b28a8f37b80070
Pedro Nora <pedro@nora.vc>
2021-02-25 17:36:33 -0300

    phone_number

---
+++ phone-number/phone_number.py
@@ -0,0 +1,18 @@
+class PhoneNumber:
+    def __init__(self, number):
+        numbers = ''.join((filter(str.isdigit, number)))
+
+        if numbers[0] == '1':
+            numbers = numbers[1:]
+        
+        if len(numbers) != 10:
+            raise ValueError('Invalid phone number format')
+
+        if numbers[0] in ['0', '1'] or numbers[3] in ['0', '1']:
+            raise ValueError('Invalid phone number format')
+
+        self.number = numbers
+        self.area_code = self.number[:3]
+
+    def pretty(self):
+        return f'({self.area_code})-{self.number[3:6]}-{self.number[6:]}'        
