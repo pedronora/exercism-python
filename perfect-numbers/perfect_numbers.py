26b5cc62e2785617add71abde5b94e3c38df5e5d
Pedro Nora <pedro@nora.vc>
2021-02-27 07:27:17 -0300

    perfect_numbers

--- perfect-numbers/perfect_numbers.py
+++ perfect-numbers/perfect_numbers.py
@@ -2,11 +2,11 @@ def classify(number):
     if number <= 0:
         raise ValueError('Number must be positive')
 
-    divisors = sum(n for n in range(1, number // 2 + 1) if number % n == 0)
+    aliquot_sum = sum(n for n in range(1, number // 2 + 1) if number % n == 0)
     
-    if divisors == number:
+    if aliquot_sum == number:
         return 'perfect'
-    elif divisors > number:
+    elif aliquot_sum > number:
         return 'abundant'
     else:
         return 'deficient'
