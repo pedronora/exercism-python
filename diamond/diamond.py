870950531acc2acd9cf3d8414cb8aba223920868
Pedro Nora <pedro@nora.vc>
2021-03-02 19:12:15 -0300

    Diamond

--- diamond/diamond.py
+++ diamond/diamond.py
@@ -2,13 +2,12 @@ def rows(letter):
     alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
     index = alphabet.index(letter)
-    slice2 = alphabet[:index+1]
-    slice1 = slice2[::-1]
-    letters = slice1 + slice2[1:]    
+    slice = alphabet[:index+1]
+    letters = slice[::-1] + slice[1:]    
 
     row = ''
     first_rows = []
-    for l in slice2:
+    for l in slice:
         for e in letters:
             if l == e:
                 row+=l
