abc8da41137caa6a86370f88e204b58d097db39b
Pedro Nora <pedro@nora.vc>
2021-03-04 08:46:56 -0300

    protein_translation

--- protein-translation/protein_translation.py
+++ protein-translation/protein_translation.py
@@ -1,10 +1,10 @@
 def proteins(strand):
-    aminoacids = {'Methionine': ['AUG'], 'Phenylalanine': ['UUU', 'UUC'], 'Leucine': ['UUA', 'UUG'], 'Serine': ['UCU', 'UCC', 'UCA', 'UCG'], 'Tyrosine': ['UAU', 'UAC'], 'Cysteine': ['UGU', 'UGC'], 'Tryptophan': ['UGG']}
+    aminoacids = {'Methionine': ['AUG'], 'Phenylalanine': ['UUU', 'UUC'], 'Leucine': ['UUA', 'UUG'], 'Serine': ['UCU', 'UCC', 'UCA', 'UCG'], 'Tyrosine': ['UAU', 'UAC'], 'Cysteine': ['UGU', 'UGC'], 'Tryptophan': ['UGG'], 'STOP': ['UAA', 'UAG', 'UGA']}
 
     translations = []
     for i in range(3, len(strand) + 1, 3):
         codon = strand[i-3: i]
-        if codon in ['UAA', 'UAG', 'UGA']:
+        if codon in aminoacids['STOP']:
             break
         for k in aminoacids:
             if codon in aminoacids[k]:
