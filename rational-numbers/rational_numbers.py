3871d3a8c438fcf414a442f6a10e9452461abfc6
Pedro Nora <pedro@nora.vc>
2021-03-06 10:44:17 -0300

    rational_numbers

--- rational-numbers/rational_numbers.py
+++ rational-numbers/rational_numbers.py
@@ -1,77 +1,51 @@
 from __future__ import division
 
-def mdc(a, b):
-    r = None
-    while r != 0:
-        r = a % b
-        a, b = b, r
-    return a
-
-def mmc(a, b):
-    return (a * b) / mdc(a, b)
-
-def simplify_fraction(n,d):
-    n = int(abs(n))
-    d = int(abs(d))
-    if n != 0:
-        for i in range(max(n, d), 0, -1):
-            if d % i == 0 and n % i == 0:
-                return i
-    return d
-
-
 class Rational:
     def __init__(self, numer, denom):
-        self.numer = numer
-        self.denom = denom
-        self.reduce()
+        gcd_value = Rational.gcd(numer, denom)
+        self.numer = numer / gcd_value
+        self.denom = denom /gcd_value
+
+    def gcd(a , b):
+        while b:
+            a, b = b, a % b
+        return a
 
     def __eq__(self, other):
         return self.numer == other.numer and self.denom == other.denom
 
     def __repr__(self):
         return '{}/{}'.format(self.numer, self.denom)
-
+    
     def __add__(self, other):
-        den = mmc(self.denom, other.denom)
-        num = (den/self.denom*self.numer) + (den/other.denom*other.numer)
-        return Rational(num, den)
+        numer = self.numer * other.denom + other.numer * self.denom
+        denom = self.denom * other.denom
+        return Rational(numer, denom)
 
     def __sub__(self, other):
-        den = mmc(self.denom, other.denom)
-        num = (den/self.denom*self.numer) - (den/other.denom*other.numer)
-        return Rational(num, den)
+        numer = self.numer * other.denom - other.numer * self.denom
+        denom = self.denom * other.denom
+        return Rational(numer, denom)
 
     def __mul__(self, other):
-        num = self.numer * other.numer
-        den = self.denom * other.denom
-        return Rational(num, den)
+        numer = self.numer * other.numer
+        denom = self.denom * other.denom
+        return Rational(numer, denom)
 
     def __truediv__(self, other):
-        num = self.numer * other.denom
-        den = self.denom * other.numer
-        return Rational(num, den)   
+        numer = self.numer * other.denom
+        denom = self.denom * other.numer
+        return Rational(numer, denom)
 
     def __abs__(self):
-        num = abs(self.numer)
-        den = abs(self.denom)
-        return Rational(num, den)
+        numer = self.numer if self.numer > 0 else self.numer * -1
+        denom = self.denom if self.denom > 0 else self.denom * -1
+        return Rational(numer, denom)
 
     def __pow__(self, power):
-        num = self.numer**power
-        den = self.denom**power
-        return Rational(num, den)
-
+        numer = self.numer ** power
+        denom = self.denom ** power
+        return Rational(numer, denom)
+    
     def __rpow__(self, base):
         return base ** (self.numer/self.denom)
-
-    def reduce(self):
-        divisor = simplify_fraction(self.numer, self.denom)
-        num = self.numer/divisor
-        den = self.denom/divisor
-        if num < 0 and den < 0:
-            num, den = num*-1, den*-1
-        if den < 0:
-            num, den = num*-1, den*-1
-        self.numer = num
-        self.denom = den
