934d106850320cbfe9d0179c545a00b48e6d9b7a
Pedro Nora <pedro@nora.vc>
2021-03-09 08:57:23 -0300

    reverse_string

--- reverse-string/reverse_string.py
+++ reverse-string/reverse_string.py
@@ -1,2 +1,6 @@
 def reverse(text):
-    return text[::-1]
+    text_list = list(text)
+    size = len(text_list)
+    for c in range(size//2):
+        text_list[c], text_list[size-1 - c] = text_list[size-1 -c], text_list[c]
+    return ''.join(text_list)
